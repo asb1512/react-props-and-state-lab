f17ff3ac2992661c7a1cf13ce39f77e0f2e07ba9
asb1512 <andrew@olympicmedia.com>
2021-07-06 21:41:18 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,11 @@ import React from 'react'
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
+const BASE_URL = '/api/pets'
+// const CAT_URL = BASE_URL + '?type=cat'
+// const DOG_URL = BASE_URL + '?type=dog'
+// const MICROPIG_URL = BASE_URL + '?type=micropig'
+
 class App extends React.Component {
   constructor() {
     super()
@@ -15,6 +20,47 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (e) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: e.target.value
+      }
+    })
+  }
+
+  onAdoptPet = (id) => {
+    let matchedPet = this.state.pets.find(e => e.id === id)
+    matchedPet.isAdopted = true
+  }
+
+  // makeFetchCall = (url) => {
+  //   fetch(url)
+  //   .then(resp => resp.json())
+  //   .then(json => console.log(json))
+  // }
+
+
+  fetchPets = () => {
+    if (this.state.filters.type === 'all') {
+      fetch(BASE_URL)
+      .then(resp => resp.json())
+      .then(json => {
+        this.setState({
+          pets: json
+        })
+      })
+    } else {
+      fetch(BASE_URL + '?type=' + this.state.filters.type)
+      .then(resp => resp.json())
+      .then(json => {
+        this.setState({
+          pets: json
+        })
+      })
+    }
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +70,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.fetchPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.onAdoptPet} pets={this.state.pets} />
             </div>
           </div>
         </div>
